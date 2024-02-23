92066598471430bef82ac365de9f3f1c5e7541f2
TejasBalkunde <tejasbalkunde1222@gmail.com>
2024-02-23 17:37:43 +0530

    just now...

---
+++ textutils/src/App.js
@@ -0,0 +1,13 @@
+import Navbar from './components/Navbar';
+import './App.css';
+
+function App() {
+  return (
+    <>
+{/* <Navbar title = {"TextUtills"} aboutText ="About TextUtills"/> */}
+<Navbar/>
+    </>
+  );
+}
+
+export default App;
---
+++ textutils/src/module2.mjs
@@ -0,0 +1,6 @@
+const a = "Tejas";
+const b = "Sam";
+const c = "Manav";
+const d = "Rutwik";
+
+export default c;
