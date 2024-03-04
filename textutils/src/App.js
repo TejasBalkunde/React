8e05e90362646cf7079958ed47a8d4170d335f5f
TejasBalkunde <tejasbalkunde1222@gmail.com>
2024-03-04 20:55:53 +0530

    React Router Setup+Usage

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -1,22 +1,37 @@
 import Navbar from './components/Navbar';
 import './App.css';
-// import Textform from './components/Textform';
+import Textform from './components/Textform';
 import About from './components/About';
-
+import{
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <>
 {/* <Navbar title = {"TextUtills"} aboutText ="About TextUtills"/> */}
+
+<Router>
 <Navbar title = {"TextUtills"} />
 {/* <Navbar/> */}
 
 <div className="container my-3">
 
-{/* <Textform heading = "Enter text to analyse"/> */}
-
-<About/>
-</div>
+<Routes>
+  {/* /users --> Component 1 (use exact always for accurate path)
+  /users/home ---> component 2 */}
+          <Route exact path="/about" element=
+          {   <About />}>
+      
+          </Route>
+          <Route exact path="/" element = {<Textform heading = "Enter text to analyse"/>}>
+          </Route>
+  </Routes>
+  </div>
+</Router>
 
     </>
   );
