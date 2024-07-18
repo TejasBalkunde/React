34cb2ef3ef27a877b38cfd9bda73b2cf134f6dae
TejasBalkunde <tejasbalkunde1222@gmail.com>
2024-07-18 11:49:54 +0530

    Updated Portfolio

---
+++ myreactapp/src/App.js
@@ -0,0 +1,18 @@
+//import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <MyName />
+      <h1 className ="myclassname"> Hello World </h1> 
+      <p>Awesome</p>
+      <MyName />
+    </>
+  )
+}
+
+const MyName = () => {
+  return <h1>My name is Tejas Balkunde</h1>
+};
+
+export default App;
